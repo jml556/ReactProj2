ebee33392ea272a3abdf7b7d059ddb3b05108e63
MrSquash14 <76232474+MrSquash14@users.noreply.github.com>
2021-09-22 18:16:13 -0400

    Updated component

---
+++ myapp/src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from './searchbar.js'
+
+const App = () => {
+    return(
+        <div>
+            <SearchBar/>
+        </div>
+    )
+}
+
+export default App
